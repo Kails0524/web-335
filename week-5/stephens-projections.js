50d3d1461db8e5b436e492049971a98cd289e9a4
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-09-11 14:25:19 -0500

    Update stephens-projections.js

--- week-5/stephens-projections.js
+++ week-5/stephens-projections.js
@@ -5,13 +5,13 @@
  */
 
 // query to add a new user 
-db.users.insertOne({"firstName": 'Antonio', "lastName" : 'Vivaldi', "employeeId": '1013', "email": 'avivaldi@me.com', "dateCreated": new Date()}));
+db.users.insertOne({"firstName": 'Antonio', "lastName" : 'Vivaldi', "employeeId": '1013', "email": 'avivaldi@me.com', "dateCreated": new Date()});
 
 // query that updates Mozart's email address 
-db.users.updateOne({"email": "wmozart@me.com"},{"$set":{"email": "mozart@me.com"}}));
+db.users.updateOne({"email": "wmozart@me.com"},{"$set":{"email": "mozart@me.com"}});
 
 //query to prove the email address was updated
-db.users.findOne({"email": "mozart@me.com"}));
+db.users.findOne({"email": "mozart@me.com"});
 
 // query that lists all the documents, displaying only the firstName, lastName, and email fields.
-db.users.aggregate([{$project: {_id: 0,firstName: 1,lastName: 1,email: 1}}]));
+db.users.aggregate([{$project: {_id: 0,firstName: 1,lastName: 1,email: 1}}]);
