b7c8945e56b110a6471159e836feccbc19cafc56
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-09-11 14:24:59 -0500

    Update stephens-projections.js

--- week-5/stephens-projections.js
+++ week-5/stephens-projections.js
@@ -1,9 +1,17 @@
-// query to add a new user to the user's collection
+/**
+ * Title: Projections
+ * Author: Kailee Stephens 
+ * Date: 09/11/2022
+ */
+
+// query to add a new user 
 db.users.insertOne({"firstName": 'Antonio', "lastName" : 'Vivaldi', "employeeId": '1013', "email": 'avivaldi@me.com', "dateCreated": new Date()}));
 
-// query that updates Mozart's email address to mozart@me.com. Second, Write a query to prove the email address was updated
+// query that updates Mozart's email address 
 db.users.updateOne({"email": "wmozart@me.com"},{"$set":{"email": "mozart@me.com"}}));
+
+//query to prove the email address was updated
 db.users.findOne({"email": "mozart@me.com"}));
 
-// query that lists all the documents in the user's collection. Only display the firstName, lastName, and email fields.
+// query that lists all the documents, displaying only the firstName, lastName, and email fields.
 db.users.aggregate([{$project: {_id: 0,firstName: 1,lastName: 1,email: 1}}]));
